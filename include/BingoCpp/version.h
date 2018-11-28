5e17ad48c003672fc976cd17f68ecf2c3320afd3
Geoffrey Bomarito <gbomarit@herman.larc.nasa.gov>
2018-11-28 15:28:10 -0500

    updated cmake build process

--- include/BingoCpp/version.h
+++ include/BingoCpp/version.h
@@ -1,3 +1,12 @@
-// the configured options and settings for Tutorial
-#define Tutorial_VERSION_MAJOR 0
-#define Tutorial_VERSION_MINOR 1
+/*
+ * This file contains definitions made in CMakeLists.txt
+ * that we want available in the source code.
+ * In the source code, include "BingoCpp/version.h" (no .in suffix).
+ * This header file will be generated, and filled with the
+ * right definition values. Change the namings as you wish,
+ * but make sure they match up with whats in CMakeLists.txt.
+ */
+#define PROJECT_VERSION_MAJOR 0
+#define PROJECT_VERSION_MINOR 1
+#define PROJECT_VERSION_PATCH 1
+#define PROJECT_VERSION_TWEAK 0
