b59d59fdad2311fd0d29ecbe852f0929e8ea1db0
Tyler Townsend <ttown@knights.ucf.edu>
2019-02-20 14:52:18 -0500

    tested forward eval functions

--- tests/backend_nodes_tests.cpp
+++ tests/backend_nodes_tests.cpp
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <cmath>
-#include <iostream>
 #include <vector>
 
 #include <Eigen/Dense>
@@ -11,64 +9,49 @@
 
 namespace {
 
-class BackendNodes : public ::testing::Test {
+class BackendNodes : public ::testing::TestWithParam<int>{
  public:
-  const double X_START =0;
-  const double X_END = 1;
+  const double X_START =-1;
+  const double X_END = 0;
   const int NUM_X_VALS = 11;
-  const int X_OP = 0;
-  const int C_OP = 1;
-  const int ADD_OP = 2;
-  const int SUB_OP = 3;
-  const int MULT_OP = 4;
-  const int DIV_OP = 5;
-  const int SIN_OP = 6;
-  const int COS_OP = 7;
-  const int EXP_OP = 8;
-  const int LOG_OP = 9;
-  const int POW_OP = 10;
-  const int ABS_OP = 11;
-  const int SQRT_OP = 12;
 
-  Eigen::ArrayXXd x_vals; 
-  Eigen::ArrayX3i command_stack; // x*x + 2x
-  Eigen::VectorXd constants;
-  Eigen::ArrayXXd forward_eval;
+  const int X_IDX = 0;
+
+  testutils::AGraphValues sample_agraph_data;
+  Eigen::ArrayXXd forward_eval_x0;
   Eigen::ArrayXXd reverse_eval;
 
   virtual void SetUp() {
-    init_x_vals(x_vals);
-    init_stack(command_stack);
-    init_constants(constants);
-    forward_eval = Eigen::ArrayXXd(command_stack.rows(), x_vals.cols());
+    sample_agraph_data = testutils::init_agraph_vals(X_START, X_END, NUM_X_VALS);
+    init_forward_eval(forward_eval_x0, sample_agraph_data);
   }
   virtual void TearDown() {}
  private:
-  void init_x_vals(Eigen::ArrayXXd &x_vals) {
-    x_vals = Eigen::ArrayXXd(NUM_X_VALS, 2);
-    x_vals.col(0) = Eigen::ArrayXd::LinSpaced(NUM_X_VALS, -1*X_END, X_START);
-    x_vals.col(1) = Eigen::ArrayXd::LinSpaced(NUM_X_VALS, X_START, X_END);
-  }
-  void init_stack(Eigen::ArrayX3i &stack) {
-    stack = Eigen::ArrayX3i(5, 3);
-    stack << 0, 0, 0,
-             1, 1, 1,
-             4, 1, 0,
-             4, 0, 0,
-             2, 2, 3;
-  }
-  void init_constants(Eigen::VectorXd &constants) {
-    constants = Eigen::ArrayXd(1);
-    constants << 2.0;
+  void init_forward_eval(Eigen::ArrayXXd &forward_eval_x0,  testutils::AGraphValues &sample_agraph_data) {
+    std::vector<Eigen::ArrayXXd> vec_eval = testutils::init_op_evals_x0(sample_agraph_data);
+    forward_eval_x0 = Eigen::ArrayXXd::Zero(vec_eval.size(), sample_agraph_data.x_vals.rows());
+    for (int i=0; i<vec_eval.size(); i++) {
+      forward_eval_x0.row(i) = vec_eval[i].transpose();
+    }
   }
 };
 
-TEST_F(BackendNodes, loadx_forward_eval) {
-  Eigen::ArrayXXd res = backendnodes::forward_eval_function(0, 0, 0, 
-                                                            x_vals, 
-                                                            constants, 
-                                                            forward_eval);
-  // ASSERT_TRUE(true);                                                            
-  ASSERT_TRUE(testutils::almost_equal(x_vals.col(0), res));
+TEST_P(BackendNodes, forward_eval) {
+  int node_op = GetParam();
+  Eigen::ArrayXXd res = backendnodes::forward_eval_function(
+    node_op, X_IDX, X_IDX, 
+    sample_agraph_data.x_vals, 
+    sample_agraph_data.constants, 
+    forward_eval_x0);
+  ASSERT_TRUE(testutils::almost_equal(forward_eval_x0.row(node_op), res));
 }
+
+// TEST_P(BackendNodes, reverse_eval) {
+//   int node_op = GetParam();
+//   backendnodes::reverse_eval_function(node_op, X_IDX, X_IDX, 
+//     forward_eval_x0);
+//   ASSERT_TRUE(testutils::almost_equal(forward_eval_x0.row(node_op), res));
+// }
+
+INSTANTIATE_TEST_CASE_P(,BackendNodes, ::testing::Range(0, 13, 1));
 }// namespace
