9bc53a21942ae4928b977dfdc62ed0895c5ae7e9
David Randall <madmathskills@gmail.com>
2022-06-07 15:38:16 -0600

    Revert "changed init to work with bingo setup.py"
    This reverts commit 8e0cd9ade09d10f7dc5522ceb8405a82f59871e7.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
-from bingo.bingocpp import Equation, \
-                           AGraph, \
-                           ExplicitRegression, \
-                           ExplicitTrainingData, \
-                           ImplicitRegression, \
-                           ImplicitTrainingData, \
-                           evaluation_backend, \
-                           simplification_backend, \
-                           FitnessFunction, \
-                           VectorBasedFunction, \
-                           TrainingData, \
-                           GradientMixin, \
-                           VectorGradientMixin
+from .build.bingocpp import Equation, \
+                            AGraph, \
+                            ExplicitRegression, \
+                            ExplicitTrainingData, \
+                            ImplicitRegression, \
+                            ImplicitTrainingData, \
+                            evaluation_backend, \
+                            simplification_backend, \
+                            FitnessFunction, \
+                            VectorBasedFunction, \
+                            TrainingData, \
+                            GradientMixin, \
+                            VectorGradientMixin
+
+del build
